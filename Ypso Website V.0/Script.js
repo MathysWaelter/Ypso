41966b4548bf0790b890028386e38ad666d32755
benjaminvandammeholberton <6206@holbertonstudents.com>
2023-07-10 19:26:43 +0200

    Add: caroussel dans la section "Portfolio"

--- Ypso Website V.0/Script.js
+++ Ypso Website V.0/Script.js
@@ -46,3 +46,45 @@ return currentSection;
 }
 
 window.addEventListener("wheel", detectScroll);
+
+
+// Slider Portfolio section
+document.addEventListener('DOMContentLoaded', function() {
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev =  document.getElementById('prev');
+
+let active = items.length % 2 !== 0 ? (items.length - 1) / 2 : items.length / 2;
+
+function loadShow(){
+  let stt = 0;
+  items[active].style.transform = `none`;
+  items[active].style.zIndex = 1;
+  items[active].style.filter = 'none';
+  items[active].style.opacity = 1;
+  for(var i = active + 1; i < items.length;  i++){
+    stt++;
+    items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = 'blur(5px)';
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+  }
+  stt = 0;
+  for(var i = active - 1;  i >=0; i--){
+    stt++;
+    items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = 'blur(5px)';
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+  }
+}
+loadShow();
+next.onclick = function(){
+  active = active + 1 < items.length ? active + 1 : active = 0;
+  loadShow();
+}
+prev.onclick = function(){
+  active = active - 1 >= 0 ? active - 1 : active = items.length - 1;
+  loadShow();
+}
+});
